b6f1334fe931d14d7b3b36fd7647da9081a051b4
Rali <ralka.ivanova@gmail.com>
2017-12-21 18:20:10 +0000

    Revert "PR title "

--- lfweb/admin.py
+++ lfweb/admin.py
@@ -1,42 +1,25 @@
 from django.contrib import admin
-from models import Collection, Yarn, News, Image, Tecnicalcard, HomepageVideo
+from models import Collection, Yarn, News, Gallery
 from modeltranslation.admin import TranslationAdmin
 
 class NewsAdmin(TranslationAdmin):
     list_display = ( 'title','abstract','text','pub','pub_order',)
-    
+    date_hierarchy = 'date'
     prepopulated_fields = {'slug':('title',),}
-    list_filter = ('pub',)
-    exclude = ('abstract',)	
     
 admin.site.register(News, NewsAdmin)
 
-class TecnicalcardInline(admin.TabularInline):
-    model = Tecnicalcard
-
 class CollectionAdmin(TranslationAdmin):
-	list_display = ('parent','name','title','abstract','pub','pub_order',)
-	prepopulated_fields = {'slug':('name',),}
-	#inlines = [TecnicalcardInline, ]
+    pass
 
 admin.site.register(Collection, CollectionAdmin)
 
 class YarnAdmin(TranslationAdmin):
-	list_display = ('name','icon','pub','pub_order','collection')
-	list_filter = ('collection','pub',)
-	exclude = ('title',)
-
-	def icon(self, obj):
-		return '<img title="%s" src="/media/%s" style="height: 42px; border-radius: 5px; -webkit-border-radius: 5px;" />' % (obj.title, obj.img)
-	icon.allow_tags = True        
+    pass
 
 admin.site.register(Yarn, YarnAdmin)
 
-class HomepageVideoAdmin(admin.ModelAdmin):
-	list_display = ('pub','video',)
-	def video(self, obj):
-		return obj.src
-	video.allow_tags = True   
+class GalleryAdmin(admin.ModelAdmin):
+    pass
 
-admin.site.register(HomepageVideo,HomepageVideoAdmin )
-
+admin.site.register(Gallery, GalleryAdmin)
--- lfweb/translation.py
+++ lfweb/translation.py
@@ -1,5 +1,5 @@
 from modeltranslation.translator import translator, TranslationOptions
-from models import Collection, Yarn, News, Tecnicalcard
+from models import Collection, Yarn, News
 
 class NewsTranslationOptions(TranslationOptions):
     fields = ('title', 'abstract','text','slug')
@@ -8,7 +8,7 @@ translator.register(News, NewsTranslationOptions)
 
 
 class CollectionTranslationOptions(TranslationOptions):
-    fields = ('name','title', 'abstract','description','slug')
+    fields = ('name','title', 'abstract')
 
 translator.register(Collection, CollectionTranslationOptions)
 
@@ -16,9 +16,4 @@ class YarnTranslationOptions(TranslationOptions):
     fields = ('name','title',)
 
 translator.register(Yarn, YarnTranslationOptions)
-
-class TecnicalcardTranslationOptions(TranslationOptions):
-    fields = ('alttext',)
-
-translator.register(Tecnicalcard, TecnicalcardTranslationOptions)
 
